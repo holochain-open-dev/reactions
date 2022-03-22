58547b94d7a4d2e6e3d6881faa7ade2a9523d16b
Matthias <tuttiacc@hotmail.com>
2022-03-22 22:52:37 +0100

    created separate zome for create_dummy_entry()

--- crates/dummy/src/lib.rs
+++ crates/dummy/src/lib.rs
@@ -1,12 +1,16 @@
-// <---  part below may be removed when using this "reactions" as an external module --->
+//! ## hc_zome_reactions_dummy
+//!
+//! Zome to create dummy entries for demo and testing of the reactions zome.
+
 use hdk::prelude::*;
 use hdk::prelude::holo_hash::EntryHashB64;
 
 mod dummy;
 use crate::dummy::DummyEntry;
 
+entry_defs![PathEntry::entry_def(), DummyEntry::entry_def()];
+
 #[hdk_extern]
 pub fn create_dummy_entry(dummy: DummyEntry) -> ExternResult<EntryHashB64> {
     dummy::create_dummy_entry(dummy)
 }
-// <--- only used for UI demo (ui/demo/index.html) --->
