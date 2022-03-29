10f8b4dfcb25c0efbe9e156d84ed49ffd0842526
Matthias <tuttiacc@hotmail.com>
2022-03-29 18:31:23 +0200

    added icon-reaction element

--- ui/src/index.ts
+++ ui/src/index.ts
@@ -1,5 +1,6 @@
 export * from './types';
 export * from './context';
+export * from './elements/icon-reaction';
 export * from './elements/choosable-emoji-reaction';
 export * from './elements/reactions-row';
 export * from './reactions-service';
